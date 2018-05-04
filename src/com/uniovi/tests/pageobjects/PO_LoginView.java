75cc41728e13d48ef5da4a60cc3f1ccfe51c1ebb
Javier C. f(z) <javitxu1995@gmail.com>
2018-05-04 12:26:48 +0200

    Son las pruebas del otro proyecto

---
+++ src/com/uniovi/tests/pageobjects/PO_LoginView.java
@@ -0,0 +1,23 @@
+package com.uniovi.tests.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PO_LoginView {
+
+	public static void fillForm(WebDriver driver, String userName, String password) {
+		WebElement username = driver.findElement(By.name("username"));
+		username.click();
+		username.clear();
+		username.sendKeys(userName);
+		WebElement pass = driver.findElement(By.name("password"));
+		pass.click();
+		pass.clear();
+		pass.sendKeys(password);
+
+		By boton = By.id("btLogin");
+		driver.findElement(boton).click();
+	}
+
+}
---
+++ src/com/uniovi/tests/pageobjects/PO_PrivateView.java
@@ -0,0 +1,23 @@
+package com.uniovi.tests.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.uniovi.tests.utils.SeleniumUtils;
+
+public class PO_PrivateView extends PO_NavView {
+
+	public static void searchUser(WebDriver driver, String userName) {
+		WebElement search = driver.findElement(By.id("search"));
+		search.click();
+		search.clear();
+		search.sendKeys(userName);
+
+		// Pulsar el boton de buscar
+		By boton = By.id("searchButton");
+		driver.findElement(boton).click();
+		SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
+}
